a9f8c654a81db9cc1f2bdb46edf0dcf94e9f7485
santanagabi <gaboliveirasantana1@gmail.com>
2023-05-25 22:28:17 -0300

    visiting a URL with cypress

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -1,1 +1,8 @@
 // cypress - Spec
+
+describe('My First Test', function() {
+    it('My First Test case', function() {
+        // test step
+        cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/#/")
+    })
+})
