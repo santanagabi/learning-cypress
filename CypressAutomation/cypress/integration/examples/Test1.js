cf25f3356bb84d08f4d9d6178c4e52ac534047f7
santanagabi <gaboliveirasantana1@gmail.com>
2023-05-30 21:47:12 -0300

    Update study about cypress

--- CypressAutomation/cypress/integration/examples/Test1.js
+++ CypressAutomation/cypress/integration/examples/Test1.js
@@ -3,6 +3,27 @@
 describe('My First Test', function() {
     it('My First Test case', function() {
         // test step
-        cy.visit("https://www.rahulshettyacademy.com/AutomationPractice/#/")
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get('.search-keyword').type('ca')
+        cy.get('.search-keyword')
+        cy.wait(2000)
+        cy.get('.product').should('have.length', 5)
+        cy.get('.product:visible').should('have.length', 4)
+
+        // Parent child
+        cy.get('.products').find('.product').should('have.length', 4)
+
+        cy.get(':nth-child(3) > .product-action > button').click()
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click()
+
+        cy.get('.products').find('.product').each(($el, index, $list) => {
+            const textVeg = $el.find('h4.product-name').text()
+            if(textVeg.includes('Cashews')) {
+                cy.wrap($el).find('button').click()
+            }
+        })
+
+        // Asynchronous
+        // Then () -> wait for the promise to be execute
     })
 })
